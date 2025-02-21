f0279ae20682c0db60d40f1f8e01da883fd40bbf
Dominik Koch <dominik@koch-bautechnik.de>
2025-02-21 14:49:13 +0100

    fix: add better logging

--- helpers/validate.go
+++ helpers/validate.go
@@ -1,6 +1,7 @@
 package helpers
 
 import (
+	"fmt"
 	"net/http"
 	"strings"
 
@@ -13,12 +14,14 @@ func Validate(r *http.Request, w http.ResponseWriter) bool {
 		mediaType := strings.ToLower(strings.TrimSpace(strings.Split(ct, ";")[0]))
 		if mediaType != "application/json" {
 			msg := "Content-Type header is not application/json"
+			fmt.Println(msg)
 			http.Error(w, msg, http.StatusUnsupportedMediaType)
 			return false
 		}
 	}
 
 	if r.Header.Get("Authorization") != config.Conf.BotList.Topgg.Auth {
+		fmt.Println("Invalid authorization")
 		http.Error(w, "Invalid authorization", http.StatusUnauthorized)
 		return false
 	}
