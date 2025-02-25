4240cb156d61be4dfb5f0080fed06ed0d4344a0c
Dominik Koch <dominik@koch-bautechnik.de>
2025-02-25 13:31:30 +0100

    feat(lists): add dscbot

---
+++ botlists/dscbot.go
@@ -0,0 +1,42 @@
+package botlists
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/Would-You-Bot/vote-logger/config"
+	"github.com/Would-You-Bot/vote-logger/helpers"
+	"github.com/Would-You-Bot/vote-logger/types"
+)
+
+func HandleDscbot(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Received vote from dsc.bot")
+
+	if !helpers.Validate(r, w, config.Conf.BotList.Dscbot.Auth) {
+		return
+	}
+
+	r.Body = http.MaxBytesReader(w, r.Body, 1048576)
+
+	dec := json.NewDecoder(r.Body)
+
+	var v types.Vote
+
+	err := dec.Decode(&v)
+	if err != nil {
+		fmt.Print(err)
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	response := helpers.GetUserData(v)
+
+	message := fmt.Sprintf("https://nightly.dsc.bot/%s/vote", v.Bot)
+
+	helpers.SendVoteWebhook(response, message)
+
+	w.WriteHeader(http.StatusOK)
+
+	fmt.Println("Vote received from " + v.User + " for " + v.Bot)
+}
--- service.go
+++ service.go
@@ -16,7 +16,7 @@ func main() {
 	
 	router := mux.NewRouter()
 	router.HandleFunc("/topgg", botlists.HandleTopgg).Methods("POST")
-	router.HandleFunc("/dscbot", botlists.HandleTopgg).Methods("POST")
+	router.HandleFunc("/dscbot", botlists.HandleDscbot).Methods("POST")
 
 	fmt.Printf("Server started on port %s\n", config.Conf.Port)
 	errListen := http.ListenAndServe(":" + config.Conf.Port, router)
